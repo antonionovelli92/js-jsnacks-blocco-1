d3ad0e7e874cbaf4cfe26b831e87a0544c6244f5
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-13 18:56:16 +0100

    add 2ex

---
+++ jsnack2/js/script.js
@@ -0,0 +1,9 @@
+/*
+SNACK 2
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa. (vietato usare includes() )
+*/
+
+// Prendo l'elemento dal dom
+const resultElement = document.getElementById('result');
+
+
