9eab64b6814b19baf393593d630c9d32d5a88cd5
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-13 16:32:05 +0100

    add ex1

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,3 +2,21 @@
 SNACK 1
 Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
 */
+
+// Prendo l'elemento dal dom
+const resultElement = document.getElementById('result');
+
+// variabile d'appoggio
+let result = '';
+
+// creo un ciclo
+
+for (let i = 1; i <= 10; i++) {
+    // chiedere all'utente 10 volte un numero
+    const askNumber = prompt('Scegli un numero?').trim();
+    result += askNumber;
+
+    // console.log(askNumber)
+}
+resultElement.innerText = result;
+// 
